b22fc1f138eb1c5cbd318d8d4170ceee3787f43f
Никита Матвеев <nik.nikita.matveev.1997@gmail.com>
2022-10-12 20:00:31 +0500

    Добавлена фигура круг

---
+++ FigureArea/Circle.cs
@@ -0,0 +1,16 @@
+﻿using FigureArea.Core;
+
+namespace FigureArea;
+
+public class Circle : BaseFigure
+{
+    public decimal R { get; init; }
+
+    public Circle(decimal r)
+    {
+        VerifyNumber(r);
+        R = r;
+    }
+
+    public override decimal Area => (decimal)Math.PI * R * R;
+}
---
+++ FigureAreaTest/CircleTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FigureArea;
+
+namespace FigureAreaTest
+{
+    public class CircleTest
+    {
+        [TestCase(1, 3.142)]
+        [TestCase(3, 28.274)]
+        public void Area_Return_right(decimal r, decimal result)
+        {
+            Circle circle = new Circle(r);
+
+            Assert.AreEqual(result, Decimal.Round(circle.Area, 3));
+        }
+    }
+}
